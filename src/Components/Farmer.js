572e33ad683c4649a3eb249d6c46972bdbf79ca5
ISURU <isuruumanga123@gmail.com>
2023-04-07 17:09:17 +0530

    update farmer page

--- src/Components/Farmer.js
+++ src/Components/Farmer.js
@@ -3,8 +3,25 @@ import "../Styles/Farmer.css";
 import img1 from "../images/dashboard-img1.svg";
 import img2 from "../images/dashboard-img2.svg";
 
-function Farmer(props) {
+export default function Farmer(props) {
 	const handleClick = props.handleClick;
+
+	//   const [post, setPost] = useState({
+	//     location: "",
+	//     description: "",
+	//     hashtag: "",
+	//   });
+
+	//   const { id } = useParams();
+
+	//   useEffect(() => {
+	//     loadPost();
+	//   }, []);
+
+	//   const loadPost = async () => {
+	//     const result = await axios.get(`http://localhost:8080/post/${id}`);
+	//     setPost(result.data);
+	//   };
 
 	return (
 		<div className='farmerPage'>
@@ -12,100 +29,121 @@ function Farmer(props) {
 			<button onClick={() => handleClick("addFarmer")}>
 				Add Farmer <i class='fa-solid fa-plus'></i>
 			</button>
-			<div className='users'>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-				<div className='user'>
-					<img
-						src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
-						alt=''
-					/>
-					<div className='details'>
-						<h2>UserName</h2>
-						<p>Farmer</p>
-					</div>
-				</div>
-			</div>
+			<table class='table table-striped'>
+				{" "}
+				<tr className='users'>
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+				</tr>
+				<tr className='users'>
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+
+					<td className='user'>
+						<img
+							src='http://health5.ru/wp-content/uploads/2018/03/Rejuvenate-the-face.jpg'
+							alt=''
+						/>
+						<div className='details'>
+							<h2>UserName</h2>
+							<p>Farmer</p>
+						</div>
+					</td>
+				</tr>
+			</table>
 		</div>
 	);
 }
 
-export default Farmer;
+// import axios from "axios";
+// import React, { useEffect,useState } from "react";
+// import { Link, useParams } from "react-router-dom";
+
+// export default function ViewPost() {
+//   const [post, setPost] = useState({
+//     location: "",
+//     description: "",
+//     hashtag: "",
+//   });
+
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     loadPost();
+//   }, []);
+
+//   const loadPost = async () => {
+//     const result = await axios.get(`http://localhost:8080/post/${id}`);
+//     setPost(result.data);
+//   };
+
+//   return (
+//     <table class="table table-striped">
+//   <tr>
+//     <tr style="font-weight:bold;">Hotel nam</tr>
+//     <tr>{post.location}</tr>
+//   </tr>
+//   <tr>
+//     <tr style="font-weight:bold;">Address</tr>
+//     <tr>   {post.description}</tr>
+//   </tr>
+//   <tr>
+//     <tr style="font-weight:bold;">Phone Nu</tr>
+//     <tr>{post.hashtag}</tr>
+//   </tr>
+
+//   <tr class="profile-actions">
+//     <tr><button><a href={"/edit/"+this.props.obj._id}>Edit</a></button></tr>
+//     <tr><button onclick={this.delete}>Delete Account</button></tr>
+//   </tr>
+// </table>
+
+//   );
+// }
